566c0daa35bd81c3b5ebbdc5bb542a960df4ca34
Abdelrahman Baz <bedomagdy10@gmail.com>
2022-04-22 22:52:51 -0500

    added some static pages.

--- DiningReviewsApplication/src/main/java/com/example/diningreviews/DiningReviewsApplication.java
+++ DiningReviewsApplication/src/main/java/com/example/diningreviews/DiningReviewsApplication.java
@@ -2,12 +2,14 @@ package com.example.diningreviews;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DiningReviewsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DiningReviewsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DiningReviewsApplication.class, args);
+    }
 
 }
